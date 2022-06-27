f72ce53e6f9f804f25f82f0cba97f98dd3e7083f
Valeri Arnaudov <99884520+valeriarnaudov@users.noreply.github.com>
2022-06-27 21:10:03 +0300

    Home page created and styled

---
+++ src/WorkOutSection/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import {
+    WorkoutBg,
+    WorkoutContainer,
+    VideoBg,
+    WorkoutContent,
+    WorkoutH1,
+    WorkoutP,
+    WorkoutBtnWrapper,
+} from "./WorkoutElements";
+import Video from "../assets/videos/video.mp4";
+
+const WorkOutSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    };
+
+    return (
+        <WorkoutContainer id="home">
+            <WorkoutBg>
+                <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+            </WorkoutBg>
+            <WorkoutContent>
+                <WorkoutH1>Working out makes you feel better</WorkoutH1>
+                <WorkoutP>
+                    Sign up today to see and share posts with elastic bands.
+                </WorkoutP>
+                <WorkoutBtnWrapper>
+                    <Button
+                        to="signup"
+                        onMouseEnter={onHover}
+                        onMouseLeave={onHover}
+                    >
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </WorkoutBtnWrapper>
+            </WorkoutContent>
+        </WorkoutContainer>
+    );
+};
+
+export default WorkOutSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import NavBar from "../NavBar";
 import Sidebar from "../SideBar";
+import WorkOutSection from "../WorkOutSection";
 
 function Home() {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,6 +14,7 @@ function Home() {
         <>
             <Sidebar isOpen={isOpen} toogle={toogle} />
             <NavBar toogle={toogle} />
+            <WorkOutSection />
         </>
     );
 }

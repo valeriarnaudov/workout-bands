ce725ad5cbcd17f473d5e2559509ee3f6c8337c9
Valeri Arnaudov <99884520+valeriarnaudov@users.noreply.github.com>
2022-07-03 11:22:55 +0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages";
 import SignIn from "./pages/SignIn";
-import NavBar from "./pages/NavBar";
+// import NavBar from "./pages/NavBar";
 
 function App() {
     return (
         <Router>
-            <NavBar />
+            {/* <NavBar /> */}
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/signin" element={<SignIn/>} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Footer from "./Footer";
 import InfoSection from "./InfoSection";
 import { homeObjOne, homeObjThree, homeObjTwo } from "./InfoSection/Data";
 import NavBar from "./NavBar";
@@ -22,6 +23,7 @@ function Home() {
             <InfoSection {...homeObjTwo}/>
             <Services />
             <InfoSection {...homeObjThree}/>
+            <Footer />
         </>
     );
 }

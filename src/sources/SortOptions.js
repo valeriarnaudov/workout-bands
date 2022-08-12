cf35c0bfe9d2d455d8f1999f7a8d39fb6a15d0dd
Valeri Arnaudov <99884520+valeriarnaudov@users.noreply.github.com>
2022-08-12 12:31:55 +0300

    Order by likes, and added ascending and descending sort

--- src/sources/SortOptions.js
+++ src/sources/SortOptions.js
@@ -1,5 +1,8 @@
 export const sortingOptions = [
     { value: "", text: "Sort by:" },
-    { value: "title", text: "Title" },
-    { value: "date", text: "Date" },
+    { value: "titleasc", text: "Title ↑" },
+    { value: "datedesc", text: "Date ↓" },
+    { value: "dateasc", text: "Date ↑" },
+    { value: "likesdesc", text: "Likes ↓" },
+    { value: "likesasc", text: "Likes ↑" },
 ];
--- src/utils/SortItems.js
+++ src/utils/SortItems.js
@@ -1,15 +1,49 @@
 export const sortData = (selectedSort, data, setResults, results) => {
-    if (selectedSort === "title") {
+    if (selectedSort === "titleasc") {
         if (results.length > 0) {
             setResults(results.sort((a, b) => a.title.localeCompare(b.title)));
         } else {
             setResults(data.sort((a, b) => a.title.localeCompare(b.title)));
         }
-    } else if (selectedSort === "date") {
+    } else if (selectedSort === "datedesc") {
         if (results.length > 0) {
             setResults(results.sort((a, b) => b.timeStamp - a.timeStamp));
         } else {
             setResults(data.sort((a, b) => b.timeStamp - a.timeStamp));
+        }
+    } else if (selectedSort === "dateasc") {
+        if (results.length > 0) {
+            setResults(results.sort((a, b) => a.timeStamp - b.timeStamp));
+        } else {
+            setResults(data.sort((a, b) => a.timeStamp - b.timeStamp));
+        }
+    } else if (selectedSort === "likesdesc") {
+        if (results.length > 0) {
+            setResults(
+                results.sort(
+                    (a, b) => Number(b.likes.length) - Number(a.likes.length)
+                )
+            );
+        } else {
+            setResults(
+                data.sort(
+                    (a, b) => Number(b.likes.length) - Number(a.likes.length)
+                )
+            );
+        }
+    } else if (selectedSort === "likesasc") {
+        if (results.length > 0) {
+            setResults(
+                results.sort(
+                    (a, b) => Number(a.likes.length) - Number(b.likes.length)
+                )
+            );
+        } else {
+            setResults(
+                data.sort(
+                    (a, b) => Number(a.likes.length) - Number(b.likes.length)
+                )
+            );
         }
     } else {
         if (results.length > 0) {
